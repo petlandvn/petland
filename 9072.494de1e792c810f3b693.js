1050beadda187aad147447aabd0b6ef991d645dd
tinhthanh <k40cntt@gmail.com>
2022-03-07 17:38:55 +0700

    Updates

---
+++ 9072.494de1e792c810f3b693.js
@@ -0,0 +1,1 @@
+(self.webpackChunkPetland=self.webpackChunkPetland||[]).push([[9072],{9072:(s,o,t)=>{"use strict";t.r(o),t.d(o,{ion_text:()=>e});var r=t(3150),c=t(7585),l=t(1269);const e=class{constructor(s){(0,r.r)(this,s)}render(){const s=(0,c.b)(this);return(0,r.h)(r.H,{class:(0,l.c)(this.color,{[s]:!0})},(0,r.h)("slot",null))}};e.style=":host(.ion-color){color:var(--ion-color-base)}"}}]);
